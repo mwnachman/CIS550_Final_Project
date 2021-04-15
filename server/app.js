cef7399636a4c4225bc22b908f6955df9d410fc7
William Whitehead <wcwhitehead@fastmail.fm>
2021-04-15 13:44:31 -0400

    Update app.js
    Finish adding queries

--- server/app.js
+++ server/app.js
@@ -6,20 +6,18 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// testing to make sure we get a response
-app.get('/genres', (req, res) => {
-  res.send({
-    genres: ['electronic', 'reggae', 'dance']
-  })
-});
-
-// trying an endpoint that queries the db
-app.get('/artistTest', routes.artistTest);
-
 /* -- PROJECT QUERIES -- */
 
 app.get('/top5', routes.top5);
 app.get('/popularByGenre/:years', routes.popularByGenre);
+app.get('/searchSong/:song', routes.searchSong);
+app.get('/searchArtist/:artist', routes.searchArtist);
+app.get('/searchAlbum/:album', routes.searchArtist);
+app.get('/searchArtistTop10/:artist', routes.searchArtistTop10);
+app.get('/searchAlbumAllSongs/:album', routes.searchAlbumAllSongs);
+app.get('/recommendSongs/:input', routes.recommendSongs);
 
+
+// comment out line below for lambda deployment
 //app.listen(3000, () => console.log(`Listening on: 3000`));
 module.exports.handler = serverless(app);
