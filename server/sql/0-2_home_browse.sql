6d8843d7174f838627d9266dfca0cad0ecc7d3a1
Holland Delany <51930686+hdelany@users.noreply.github.com>
2021-04-23 18:24:30 -0400

    Q1: adding album ratings

--- server/sql/0-2_home_browse.sql
+++ server/sql/0-2_home_browse.sql
@@ -10,7 +10,7 @@ Randomly generate 5 song/album/artist entries in a selected genre.
 This is for the Homepage.
 Input: 		Integer representing genre code (0-8)
 Return: 	song_name, song_id, artist_name, artist_id, album_name,
-			album_id, album_release_year, album_format, record_label_name
+		album_id, album_release_year, album_format, record_label_name
 
 Note: Change "2" (genre_id) to ${genre_id} for final report.
 
@@ -53,7 +53,8 @@ Input is a specific genre.
 This will be used for the Browsing by Popularity tab.
 Input: 		Integer representing genre code (0-8)
 Return: 	album_release_year, score_rank, album_name, album_id,
-			artist_name, artist_id, album_format, record_label_name
+		artist_name, artist_id, album_format, record_label_name,
+		album_critic_score (0-10), album_user_score(0-10)
 
 Note: Change "2" (genre_id) to ${genre_id} for final report.
 
@@ -74,6 +75,8 @@ FROM (
 		, t2.artist_id
 		, t2.format AS album_format
 		, t4.name AS record_label_name
+        , t2.aoty_critic_score*.1 AS album_critic_score
+		, t2.aoty_user_score*.1 AS album_user_score
 	FROM 
 		Album t2 
 		JOIN Artist t3 ON t2.artist_id = t3.id
