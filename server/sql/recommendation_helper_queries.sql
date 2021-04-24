cd8ee62a4d54bce980686ea5db2ed1911624c1b3
Holland Delany <51930686+hdelany@users.noreply.github.com>
2021-04-23 21:26:54 -0400

    updating algorithm to be influenced by user inputs

--- server/sql/recommendation_helper_queries.sql
+++ server/sql/recommendation_helper_queries.sql
@@ -6,63 +6,62 @@ SELECT * FROM Song LIMIT 20;
 /* Find min/max values used for analysis*/
 SELECT 
 	MAX(time_signature), 
-    MIN(time_signature),
+	MIN(time_signature),
 	MAX(duration_ms), 
-    MIN(duration_ms),
-    MAX(song_key), 
-    MIN(song_key),
+	MIN(duration_ms),
+	MAX(song_key), 
+	MIN(song_key),
 	MAX(danceability), 
-    MIN(danceability), 
-    MAX(energy), 
-    MIN(energy), 
-    MAX(loudness), 
-    MIN(loudness), 
-    MAX(acousticness), 
-    MIN(acousticness), 
-    MAX(speechiness), 
-    MIN(speechiness), 
-    MAX(instrumentalness), 
-    MIN(instrumentalness), 
-    MAX(liveness), 
-    MIN(liveness), 
-    MAX(tempo), 
-    MIN(tempo),
-    MAX(valence), 
-    MIN(valence)
+	MIN(danceability), 
+	MAX(energy), 
+	MIN(energy), 
+	MAX(loudness), 
+	MIN(loudness), 
+	MAX(acousticness), 
+	MIN(acousticness), 
+	MAX(speechiness), 
+	MIN(speechiness), 
+	MAX(instrumentalness), 
+	MIN(instrumentalness), 
+	MAX(liveness), 
+	MIN(liveness), 
+	MAX(tempo), 
+	MIN(tempo),
+	MAX(valence), 
+	MIN(valence)
 FROM Song;
 SELECT 
 	MAX(release_year), 
-    MIN(release_year),
-    MAX(aoty_critic_score), 
-    MIN(aoty_critic_score),
-    MAX(aoty_user_score), 
-    MIN(aoty_user_score)
+	MIN(release_year),
+	MAX(aoty_critic_score), 
+	MIN(aoty_critic_score),
+	MAX(aoty_user_score), 
+	MIN(aoty_user_score)
 FROM Album;
  
 
 
-/* 
-manipulating algorithm values
-*/
+/* Manipulating algorithm values (all user inputs = 5) */
 SELECT 
 	t1.id AS song_id
 	, t1.name AS song_name
-	, ABS(( Input_song.danceability - t1.danceability) * 3) AS danceability_diff
-	, ABS(( Input_song.energy - t1.energy) * 2)  AS energy_diff
-	, ABS(( Input_song.loudness - t1.loudness) * 0.0157) AS loudness_diff
-	, ABS(( Input_song.acousticness - t1.acousticness) *.5) AS acousticness_diff
-	, ABS( Input_song.speechiness - t1.speechiness) AS speechiness_diff
-	, ABS(( Input_song.instrumentalness - t1.instrumentalness) * 2) AS instrumentalness_diff
-	, ABS( Input_song.liveness - t1.liveness) AS liveness_diff
-	, ABS(( Input_song.tempo - t1.tempo ) * .004)  AS tempo_diff
-	, CASE WHEN (Input_song.artist_id = t2.artist_id) THEN -4 ELSE 0 END AS same_artist_adjustment
-	, CASE WHEN (Input_song.album_id = t1.album_id) THEN -1 ELSE 0 END AS same_album_adjustment
-	, CASE WHEN (Input_song.genre_id = t2.genre_id) THEN -1 ELSE 0 END AS same_genre_adjustment
-	, CASE WHEN (Input_song.record_label_id = t2.record_label_id ) THEN -1 ELSE 0 END AS same_record_label_adjustment
-	, CASE WHEN (Input_song.album_format = t2.format ) THEN -.1 ELSE 0 END AS same_album_format_adjustment
-	, ABS(( Input_song.album_release_year - t2.release_year)*.0725) AS release_year_diff
-	, ABS(( Input_song.album_aoty_critic_score - t2.aoty_critic_score) *.01) AS aoty_critic_score_diff
-	, ABS(( Input_song.album_aoty_user_score - t2.aoty_user_score) *.01) AS aoty_critic_user_diff
+	, ABS(( Input_song.danceability - t1.danceability) * 5) AS danceability_diff
+	, ABS(( Input_song.energy - t1.energy) * 5)  AS energy_diff
+	, ABS(( Input_song.loudness - t1.loudness) * 0.0157 * 5) AS loudness_diff
+	, ABS(( Input_song.acousticness - t1.acousticness) * 5) AS acousticness_diff
+	, ABS(( Input_song.speechiness - t1.speechiness) * 5) AS speechiness_diff
+	, ABS(( Input_song.instrumentalness - t1.instrumentalness) * 5) AS instrumentalness_diff
+	, ABS(( Input_song.liveness - t1.liveness) * 5)  AS liveness_diff
+	, ABS(( Input_song.tempo - t1.tempo ) * 0.0041 * 5)  AS tempo_diff
+	, ABS(( Input_song.valence - t1.valence ) * 5)  AS valence_diff
+	, ABS(( Input_song.album_release_year - t2.release_year) * 0.0145 * 5) AS release_year_diff
+	, ABS(( Input_song.album_aoty_critic_score - t2.aoty_critic_score) * 0.01 * 5) AS aoty_critic_score_diff
+	, ABS(( Input_song.album_aoty_user_score - t2.aoty_user_score) * 0.01 * 5) AS aoty_critic_user_diff
+	, CASE WHEN (Input_song.artist_id = t2.artist_id) THEN -0.1 * 5 ELSE 0 END AS same_artist_adjustment
+	, CASE WHEN (Input_song.album_id = t1.album_id) THEN -0.1 * 5 ELSE 0 END AS same_album_adjustment
+	, CASE WHEN (Input_song.genre_id = t2.genre_id) THEN -0.1 * 5 ELSE 0 END AS same_genre_adjustment
+	, CASE WHEN (Input_song.record_label_id = t2.record_label_id ) THEN -0.1 * 5 ELSE 0 END AS same_record_label_adjustment
+	, CASE WHEN (Input_song.album_format = t2.format ) THEN -0.1 * 5 ELSE 0 END AS same_album_format_adjustment
 FROM 
 	Song t1 
 	LEFT JOIN Album t2 ON t1.album_id = t2.id
@@ -80,6 +79,7 @@ FROM
 			, t1.instrumentalness AS instrumentalness
 			, t1.liveness AS liveness
 			, t1.tempo AS tempo
+			, t1.valence AS valence
 			, t2.title AS album_name
 			, t2.artist_id AS artist_id
 			, t2.genre_id AS genre_id
@@ -96,4 +96,6 @@ FROM
 	) Input_song
 WHERE t1.id != Input_song.song_id
 LIMIT 50;
+
+
 
