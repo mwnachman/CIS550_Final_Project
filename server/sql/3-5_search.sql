974d99b49229f8a856762fbb3f4a4d2459076271
Holland Delany <51930686+hdelany@users.noreply.github.com>
2021-04-27 02:42:21 -0400

    added fuzzy search

--- server/sql/3-5_search.sql
+++ server/sql/3-5_search.sql
@@ -6,7 +6,6 @@ SELECT * FROM Song LIMIT 20;
 
 /* 
 Query 3
-
 Search a song by keyword. This will be used in our Search tab. 
 Exact matches are listed first.
 Input: 		Keyword to represent song name
@@ -14,8 +13,6 @@ Return: 	song_name, song_id, album_name, artist_name, album_release_year
  
 Note:	Change "Hello" (t1.name, song_name) with ${user_keyword} for final report, 
 	note that some ORDER BY statements have spaces in them.
-
-ADD FUZZY SEARCH
 */
 
 
@@ -31,11 +28,13 @@ FROM Song t1
 	LEFT JOIN Album t2 ON t1.album_id = t2.id
 	LEFT JOIN Artist t3 ON t2.artist_id = t3.id
 WHERE t1.name LIKE '%Hello%'
+	OR SOUNDEX(t1.name) = SOUNDEX('Hello')
 ORDER BY 
 	(song_name = 'Hello') DESC 
 	, (song_name LIKE 'Hello %') 
 		OR (song_name LIKE '% Hello') 
 		OR (song_name LIKE '% Hello %') DESC
+	, (song_name LIKE '%Hello %') DESC
 	, length(song_name);
     
     
@@ -54,6 +53,7 @@ FROM (
 		, id 
 	FROM Song
 	WHERE Song.name LIKE '%Hello%'
+		OR SOUNDEX(Song.name) = SOUNDEX('Hello')
 ) t1 
 	LEFT JOIN Album t2 ON t1.album_id = t2.id
 	LEFT JOIN Artist t3 ON t2.artist_id = t3.id
@@ -62,13 +62,13 @@ ORDER BY
 	, (song_name LIKE 'Hello %') 
 		OR (song_name LIKE '% Hello') 
 		OR (song_name LIKE '% Hello %') DESC
+	, (song_name LIKE '%Hello %') DESC
 	, length(song_name);
   
   
  
 /* 
 Query 4
-
 Search for an artist by keyword. Include the number of songs and albums they have. 
 This will be used in our Search tab. Exact matches are listed first.
 Input: 		Keyword to represent artist name
@@ -77,9 +77,6 @@ Return: 	artist_name, artist_id, album_count, song_count
  
 Note:	Change "Michael" (t3.name, artist) with ${user_keyword} for final report,
 	note that some ORDER BY statements have spaces in them.
-
-ADD FUZZY SEARCH
-NEEDS OPTIMIZATION
 */
   
 
@@ -100,6 +97,7 @@ WITH similar_artists  AS (
 			LEFT JOIN Album t2 ON t1.album_id = t2.id
 			LEFT JOIN Artist t3 ON t2.artist_id = t3.id
 		WHERE t3.name LIKE '%Michael%' 
+			OR SOUNDEX(t3.name) = SOUNDEX('Michael')
 	) x
 	GROUP BY artist_name
 ), 
@@ -141,26 +139,25 @@ FROM similar_artists
 	LEFT JOIN Genre t5 ON most_frequent_genre.genre_id = t5.id
 ORDER BY 
 		(artist_name LIKE 'Michael %') DESC 
-		, (artist_name LIKE '% Michael') DESC 
+		, (artist_name LIKE '% Michael') DESC
+		, (artist_name LIKE '%Michael%') DESC
 		, album_count DESC 
 		, song_count DESC;
 
 
 
+
+
+
 /* 
 Query 5
-
 Search for an album by keyword. This will be used in our Search tab.
 Exact matches are listed first.
 Input: 		Keyword to represent album name
 Return: 	album_name, album_id, artist_name, album_release_year,
 			album_format, record_label_name
-
 Note:	Change "Night" (t1.title, album_name) with ${user_keyword} for final report, 
 	note that some ORDER BY statements have spaces in them.
- 
-ADD FUZZY SEARCH
-NEEDS OPTIMIZATION
 */
 
 
@@ -176,14 +173,11 @@ FROM Album t2
 	LEFT JOIN Artist t3 ON t2.artist_id = t3.id
 	LEFT JOIN  RecordLabel t4 ON t2.record_label_id = t4.id
 WHERE t2.title LIKE '%Night%'
+	OR SOUNDEX(t2.title) = SOUNDEX('Night')
 ORDER BY 
 	(album_name = 'Night') DESC 
 	, (album_name LIKE 'Night %') 
 		OR (album_name LIKE '% Night') 
 		OR (album_name LIKE '% Night %') DESC
+	, (album_name LIKE '%Night%')  DESC
 	, length(album_name);
-    
-
-
-
-    
