1dfa547b629c40ef237fe99be33a5403edc095fa
William Whitehead <wcwhitehead@fastmail.fm>
2021-04-28 15:39:37 -0400

    Update lambda_function.py

--- artwork_lambda/lambda_function.py
+++ artwork_lambda/lambda_function.py
@@ -9,14 +9,15 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 def lambda_handler(event, context):
   
-  os.environ['SPOTIPY_CLIENT_ID'] = ''
-  os.environ['SPOTIPY_CLIENT_SECRET'] = ''
-  os.environ['SPOTIPY_REDIRECT_URI'] = ''
+  os.environ['SPOTIPY_CLIENT_ID'] = 'a5f55adbfc4a47f5ab95b106504958d3'
+  os.environ['SPOTIPY_CLIENT_SECRET'] = '101395bebefd48928bbda0567f9454f5'
+  os.environ['SPOTIPY_REDIRECT_URI'] = 'http://google.com/'
   
   spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
   artist_id = event['queryStringParameters']['artist']
   album_id = event['queryStringParameters']['album']
+  song_id = event['queryStringParameters']['song']
 
   body = {}
 
@@ -31,8 +32,13 @@ def lambda_handler(event, context):
     album_results = spotify.album(album_uri)
     album_image = album_results['images'][0]
     body.update({'album_image': album_image})
+  
+  if len(song_id) > 0:
+    song_uri = 'spotify:track:' + song_id
+    song_results = spotify.track(song_uri)
+    song_preview = song_results['preview_url']
+    body.update({'song_preview': song_preview})
 
   response = { 'statusCode': 200, 'headers': { 'Content-Type': 'application/json' }, 'body': json.dumps(body) }
 
   return response
-
